eac870aba29a1c8cbe8c7115101b612f78195a4b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-23 23:52:46 +0100

    test: add new test

--- unit.test.js
+++ unit.test.js
@@ -6,7 +6,13 @@ import LinearEquation from "./linearEquation";
 describe("Solve linear equations", () => {
     test("7x - 2 = 21 is equal to 23/7", () => {
         const equation = "7x - 2 = 21";
-        const solver = new LinearEquation("7x - 2 = 21");
+        const solver = new LinearEquation(equation);
         expect(solver.solve(equation)).toBe(23 / 7);
+    });
+
+    test("2(4x + 3) + 6 = 24 -4x is equal to 1", () => {
+        const equation = "2(4x + 3) + 6 = 24 -4x";
+        const solver = new LinearEquation(equation);
+        expect(solver.solve(equation)).toBe(1);
     });
 });
