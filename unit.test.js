437699ed0f619814bacb86c3a8a82ca8b959eaa5
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-23 23:47:01 +0100

    test: add a test

--- unit.test.js
+++ unit.test.js
@@ -7,6 +7,6 @@ describe("Solve linear equations", () => {
     test("7x - 2 = 21 is equal to 23/7", () => {
         const equation = "7x - 2 = 21";
         const solver = new LinearEquation("7x - 2 = 21");
-        expect(solver.solve(equation)).toBe("23/7");
+        expect(solver.solve(equation)).toBe(23 / 7);
     });
 });
